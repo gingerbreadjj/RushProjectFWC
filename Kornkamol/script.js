bfcb4e3528db4bad12bb9191d197d45ab9dd99de
bamkornkamol <88421920+bamkornkamol@users.noreply.github.com>
2024-02-01 20:24:58 +0700

    resume

---
+++ Kornkamol/script.js
@@ -0,0 +1,29 @@
+function kao(){
+    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951303232462898/e036a4f381576b6c.jpg";
+    document.getElementById("namef").innerHTML = "ยำไก่ทอด";
+    document.getElementById("exfood").innerHTML = "กินได้ทุกวันไม่มีเบื่อ";
+}
+
+function wan(){
+    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951303475728414/1.jpg";
+    document.getElementById("namef").innerHTML = "ช็อคโกแลต";
+    document.getElementById("exfood").innerHTML = "3 ครั้งหลังอาหาร";
+}
+
+function nam(){
+    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951302695596172/1846e76e3dc0a504fab01dfde790b6b8.jpg";
+    document.getElementById("namef").innerHTML = "โค้ก";
+    document.getElementById("exfood").innerHTML = "เครื่องมือฮีลใจ";
+}
+
+function pak(){
+    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951572724879360/3.jpg";
+    document.getElementById("namef").innerHTML = "เห็ด";
+    document.getElementById("exfood").innerHTML = "อร่อยทุกวัน กี่วันก็ยังอร่อย";
+}
+
+function fruit(){
+    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951302943047700/2.jpg";
+    document.getElementById("namef").innerHTML = "องุ่น";
+    document.getElementById("exfood").innerHTML = "1 พวง 10 นาที";
+}
