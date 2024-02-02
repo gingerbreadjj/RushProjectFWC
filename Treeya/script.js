f30bb6ededd0341ed6fd28570d4be91b86d7448d
gingerbreadjj <152255415+gingerbreadjj@users.noreply.github.com>
2024-02-02 17:21:41 +0700

    all jj

--- Treeya/script.js
+++ Treeya/script.js
@@ -1,10 +1,11 @@
 const imageContainer = document.getElementById("imgcon");
-const image2 = document.getElementById("img2");
+const images = document.querySelectorAll(".imgprofile");
 
-imageContainer.addEventListener("mouseenter", () => {
-    image2.style.opacity = "1"; // เมื่อ hover ให้ opacity เป็น 1
-});
+let currentImageIndex = 0;
 
-imageContainer.addEventListener("mouseleave", () => {
-    image2.style.opacity = "0"; // เมื่อไม่ hover ให้ opacity เป็น 0
+imageContainer.addEventListener("click", () => {
+    images[currentImageIndex].style.opacity = "0"; // เมื่อคลิกให้ opacity เป็น 0 ของรูปปัจจุบัน
+
+    currentImageIndex = (currentImageIndex + 1) % images.length; // เปลี่ยนรูป
+    images[currentImageIndex].style.opacity = "1"; // เปลี่ยน opacity เป็น 1 ของรูปใหม่
 });
