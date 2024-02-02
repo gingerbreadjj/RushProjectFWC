65276a51ec9d4c22d3020ba4d7b19dc5ec608b53
gingerbreadjj <152255415+gingerbreadjj@users.noreply.github.com>
2024-02-02 16:23:21 +0700

    c

--- Treeya/script.js
+++ Treeya/script.js
@@ -1,29 +1,10 @@
-function kao(){
-    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951303232462898/e036a4f381576b6c.jpg";
-    document.getElementById("namef").innerHTML = "ยำไก่ทอด";
-    document.getElementById("exfood").innerHTML = "กินได้ทุกวันไม่มีเบื่อ";
-}
+const imageContainer = document.getElementById("imgcon");
+const image2 = document.getElementById("img2");
 
-function wan(){
-    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951303475728414/1.jpg";
-    document.getElementById("namef").innerHTML = "ช็อคโกแลต";
-    document.getElementById("exfood").innerHTML = "3 ครั้งหลังอาหาร";
-}
+imageContainer.addEventListener("mouseenter", () => {
+    image2.style.opacity = "1"; // เมื่อ hover ให้ opacity เป็น 1
+});
 
-function nam(){
-    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951302695596172/1846e76e3dc0a504fab01dfde790b6b8.jpg";
-    document.getElementById("namef").innerHTML = "โค้ก";
-    document.getElementById("exfood").innerHTML = "เครื่องมือฮีลใจ";
-}
-
-function pak(){
-    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951572724879360/3.jpg";
-    document.getElementById("namef").innerHTML = "เห็ด";
-    document.getElementById("exfood").innerHTML = "อร่อยทุกวัน กี่วันก็ยังอร่อย";
-}
-
-function fruit(){
-    document.getElementById("imgfood").src = "https://cdn.discordapp.com/attachments/863119521807073280/1015951302943047700/2.jpg";
-    document.getElementById("namef").innerHTML = "องุ่น";
-    document.getElementById("exfood").innerHTML = "1 พวง 10 นาที";
-}
+imageContainer.addEventListener("mouseleave", () => {
+    image2.style.opacity = "0"; // เมื่อไม่ hover ให้ opacity เป็น 0
+});
